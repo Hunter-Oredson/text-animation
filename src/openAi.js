9a81fedd6e7f01afe584134bde4ec61ea55a8a93
Sue <57229630+vanvsue001@users.noreply.github.com>
2023-06-22 22:31:45 -0700

    returns template text instead of template title, openAi returns index of template titles instead of the title it's self

--- src/openAi.js
+++ src/openAi.js
@@ -1,6 +1,62 @@
 import { allTemplates } from "./templates";
 const apiKey = "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";
 
+export async function aiSelectTemplate(prompt) {
+  console.log("hit aiSelectTemplate");
+  const url = "https://api.openai.com/v1/chat/completions";
+
+  let templateTitle = "";
+
+  const titles = allTemplates.map((item) => item.title);
+  const stringTemplatesTitles = titles.join(", ");
+
+  const userMessage = `given the array of templates: [${titles}] best suited for this prompt : ${prompt}`;
+  console.log(userMessage);
+
+  // Prepare conversation history with user message and templates
+  const conversation = [
+    { role: "system", content: "You are the user." },
+    {
+      role: "user",
+      content: "your response should include a single number and no other text",
+    },
+    {
+      role: "assistant",
+      content: userMessage,
+    },
+  ];
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  const requestBody = {
+    model: "gpt-3.5-turbo-0613",
+    messages: conversation,
+    max_tokens: 1,
+    temperature: 0, //reduce randomness
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(requestBody),
+    });
+    const data = await response.json();
+    console.log(data);
+    const reply = data.choices[0].message.content;
+    console.log(`ai reply: ${reply}`);
+    console.log(
+      `ai title selection: \n${titles[reply]} \n${allTemplates[reply].title}`
+    );
+    return allTemplates[reply].text;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // Throw the 'error' to be caught by the caller
+  }
+}
+
 export async function apiValid() {
   const url = "https://api.openai.com/v1/engines";
 
@@ -23,59 +79,4 @@ export async function apiValid() {
     .catch((error) => {
       console.error("Error:", error);
     });
-}
-
-export async function aiSelectPrompt(prompt) {
-  console.log("hit aiSelectPrompt");
-  const url = "https://api.openai.com/v1/chat/completions";
-
-  let templateTitle = "";
-
-  // const prompt =
-  //   "Advanced double digit subtraction (21 - 17) using 2 and 12 and 8";
-
-  const titles = allTemplates.map((item) => item.title);
-  const stringTemplatesTitles = titles.join(", ");
-
-  const userMessage =
-    "help me choose the template best suited for this prompt and only return the template name and no other text" +
-    " prompt: " +
-    prompt +
-    " template: " +
-    stringTemplatesTitles;
-  console.log(userMessage);
-
-  // Prepare conversation history with user message and templates
-  const conversation = [
-    { role: "system", content: "You are the user." },
-    {
-      role: "user",
-      content: userMessage,
-    },
-  ];
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${apiKey}`,
-  };
-
-  const requestBody = {
-    model: "gpt-3.5-turbo",
-    messages: conversation,
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(requestBody),
-    });
-    const data = await response.json();
-    console.log(data);
-    const reply = data.choices[0].message.content;
-    console.log(reply);
-    return reply; // Return the 'reply' value
-  } catch (error) {
-    console.error("Error:", error);
-    throw error; // Throw the 'error' to be caught by the caller
-  }
 }
